39e1f788d4e9c388709eea7d18df9f3c701398ba
Polonsky13 <a.polonsky13@yandex.ru>
2022-06-02 11:03:44 +0300

    02-modal

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,19 +1,22 @@
-(function() {
-    const close = document.getElementById('modal-close');
-    const modal = document.getElementById('modal');
-    const modalOpen = document.getElementById('modal-open');
+(function () {
+  const close = document.getElementById('modal-close');
+  const modal = document.getElementById('modal');
+  const modalOpen = document.getElementById('modal-open');
+  const background = document.getElementById('blackout')
 
-    if (!close || !modalOpen || !modal) {
-        return;
-    }
+  if (!close || !modalOpen || !modal) {
+    return;
+  }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
-        document.body.overflow = 'hidden';
-    });
+  modalOpen.addEventListener('click', () => {
+    modal.style.display = 'block';
+    document.body.overflow = 'hidden';
+    background.style.display = 'block';
+  });
 
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
-        document.body.overflow = 'initial';
-    });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    background.style.display = 'none';
+    document.body.overflow = 'initial';
+  });
 })();
