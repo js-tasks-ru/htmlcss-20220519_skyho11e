99668a30295884dcf10d3c8aeddf40c42ab1aa87
Polonsky13 <a.polonsky13@yandex.ru>
2022-06-02 21:24:48 +0300

    02-modal: discussion

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,21 +2,18 @@
   const close = document.getElementById('modal-close');
   const modal = document.getElementById('modal');
   const modalOpen = document.getElementById('modal-open');
-  const background = document.getElementById('blackout')
 
   if (!close || !modalOpen || !modal) {
     return;
   }
 
   modalOpen.addEventListener('click', () => {
-    modal.style.display = 'block';
+    modal.style.display = 'flex';
     document.body.overflow = 'hidden';
-    background.style.display = 'block';
   });
 
   close.addEventListener('click', () => {
     modal.style.display = 'none';
-    background.style.display = 'none';
     document.body.overflow = 'initial';
   });
 })();
